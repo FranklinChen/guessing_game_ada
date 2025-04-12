891881ac2efa41445669e0c8615f196fe12d6e91
Franklin Chen <franklinchen@franklinchen.com>
2025-04-11 20:35:23 -0400

    Initial.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Guessing game, in Ada, as implemented by [kqr](https://entropicthoughts.com/guessing-game-ada-style)
+
+![CI](https://github.com/FranklinChen/guessing_game_ada/actions/workflows/ci.yml/badge.svg)
+
+All I did was copy the author's code into an Alire project.
---
+++ src/guessing_game.adb
@@ -0,0 +1,44 @@
+with Ada.Text_IO;
+with Ada.Numerics.Discrete_Random;
+
+procedure Guessing_Game is
+   subtype Small_Number is Integer range 1 .. 100;
+
+   package Random_Secret is new Ada.Numerics.Discrete_Random (Small_Number);
+   use Ada.Text_IO;
+
+   Seed : Random_Secret.Generator;
+   Secret_Number : Small_Number;
+begin
+   Random_Secret.Reset (Seed);
+   Secret_Number := Random_Secret.Random (Seed);
+
+   Put_Line ("Guess the number");
+
+   Game : loop
+      Put_Line ("Please input your guess.");
+
+      begin
+         declare
+            Input : constant String := Get_Line;
+            Guess : constant Small_Number := Small_Number'Value (Input);
+         begin
+            Put_Line ("You guessed: " & Small_Number'Image (Guess));
+
+            if Guess < Secret_Number then
+               Put_Line ("Your guess was too low!");
+            elsif Guess > Secret_Number then
+               Put_Line ("Your guess was too high!");
+            else
+               Put_Line ("Wow, you guessed right!");
+               exit Game;
+            end if;
+         end;
+      exception
+         when CONSTRAINT_ERROR =>
+            null;
+      end;
+   end loop Game;
+
+   Put_Line ("The secret number was: " & Small_Number'Image (Secret_Number));
+end Guessing_Game;
